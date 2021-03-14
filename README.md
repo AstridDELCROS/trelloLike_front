66991b3637b4b87b822a159d19ddcb5dd38a33da
Astrid Delcros <astriddelcros@gmail.com>
2021-03-14 18:28:54 +0100

    récup et modif via API

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-### Etape 1
+### Etape 1 : dynamiser les modals pour créer des listes et des cartes
 
 Pour ouvrir le site dans un navigateur :
 $ open -a 'Google Chrome' index.html
 
-Intégration avec [lien](https://bulma.io/)
+Intégration avec [Bulma](https://bulma.io/)
 
-**Dynamiser les modals pour créer des listes et des cartes**
 - Fonction de gestion des écouteurs pour afficher/cacher les modals : `addListenerToActions`.
 - Création d'un template dans le HTML avec un id spécifique (list-id), puis d'une méthode `makeListInDOM` qui récupère le template et l'envoie dans `handleAddListForm`, appelée lors de la soumission du formulaire de création de liste.
 - Création d'une modal pour ajouter une carte avec id (list_id) dans le form et input hidden.
@@ -14,4 +13,17 @@ Intégration avec [lien](https://bulma.io/)
 - Pour lier la carte à la bonne liste -> ciblage de `.panel` (la liste) le plus proche avec "event.target.closest", récupération de l'id de la liste à envoyer dans le champ id de card.
 - Désormais, la modal d'ajout de carte s'affiche, donc écouteur sur "submit" qui lance `handleAddCardForm`.
 - `handleAddCardForm` envoie à `makeCardInDOM` deux paramètres : name="content" (de la modal card) et list_id qui a été remplacée par l'id de la liste dans `showAddCardModal`.
-- Même logique pour `makeCardInDOM` qu'avec `makeListInDOM` -> récupération du template, titre générique remplacé par celui entré dans le form et ajout de la carte dans la bonne list.
+- Même logique pour `makeCardInDOM` qu'avec `makeListInDOM` -> récupération du template, titre générique remplacé par celui entré dans le form et ajout de la carte dans la bonne liste.
+
+
+### Etape 2 : récupération des listes via API et modification de la bdd depuis front
+
+- Ajout d'une propriété `base_url` qui contient l'url de l'API (son point d'entrée de base).
+- Puis appel AJAX sur cette url avec fetch et await la réponse en format JSON pour récupérer les données sous forme de tableau.
+- Boucle sur tableau pour appeler `makeListInDOM` et créer chacune des listes.
+- Boucle card dans boucle liste pour ajouter les cartes.
+- Sélection '.box' du template card pour modifier background et lui attribuer card.color définie dans bdd (si modification de couleur ou position dans bdd, ne pas oublier de relancer avec `psql -U astrid -d trello -f ./data/create_db.sql`).
+- Appel API dans `handleAddListForm` pour envoyer les listes créées dans le form à la bdd
+- Ajout du middleware multer en back pour gérer le format de formData
+- Modification du titre via form hidden, écouteur pour déclencher `showFormToEditTitle` dans `makeListInDOM`
+- Listener sur submit lance `handleEditListForm` qui envoie les données dans la bdd avec fetch patch
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,41 @@
 const app = {
 
-    init: function () {
+    testAjax: async function() {
+        const response = await fetch('https://swapi.dev/api/people/1');
+        // interpréation du résultat en json
+        const content = await response.json();
+        console.log(content);
+    },
+
+    base_url: 'http://localhost:4000',
+
+    getListsFromAPI: async function() {
+        try {
+            const response = await fetch(`${app.base_url}/lists`);
+            if (response.status !== 200) {
+                const error = await response.json();
+                throw Error(error);
+            }
+            const lists = await response.json();
+            // console.log(lists);
+            for (const list of lists) {
+                app.makeListInDOM(list.name, list.id);
+                for (const card of list.cards) {
+                    app.makeCardInDOM(card);
+                }
+            }
+        } catch (error) {
+            alert('désolé, un problème est survenu')
+            console.log(error);
+        }
+    },
+
+    init: function() {
         console.log('app.init !');
         app.addListenerToActions();
-      },
+        // app.testAjax();
+        app.getListsFromAPI();
+    },
     
     addListenerToActions: function() {
         const getAddListButton = document.getElementById('addListButton');
@@ -40,7 +72,7 @@ const app = {
         // Mise à jour de la valeur du champs list_id
         const listIdField = addCardModal.querySelector('[name="list_id"]');
         listIdField.value = listId;
-      },
+    },
 
     hideModals: function() {
         const modals = document.querySelectorAll('.modal');
@@ -49,26 +81,60 @@ const app = {
         }
     },
 
-    handleAddListForm: function(event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        // console.log(formData.get('name'));
-        app.makeListInDOM(formData.get('name'));
+    handleAddListForm: async function(event) {
+        try {
+            event.preventDefault();
+            const formData = new FormData(event.target);
+            // Appel à l'API pour envoyer les données à sauvegarder (une liste)
+            // FormData utilise le format `multipart/form-data` non géré par Express, il faut ajouter multer en back
+            const response = await fetch(`${app.base_url}/lists`, {
+                method: 'post',
+                body: formData
+            });
+            const newListOrError = await response.json();
+            if(response.status !== 200){
+                throw Error(newListOrError);
+            }
+            // app.makeListInDOM(formData.get('name'));
+            app.makeListInDOM(newListOrError.name, newListOrError.id);
+        } catch (error) {
+            alert('désolé, une erreur est survenue..');
+            console.error(error);
+        }
     },
 
-    handleAddCardForm: function (event) {
+    handleAddCardForm: async function(event) {
         event.preventDefault();
         const formData = new FormData(event.target);
         app.makeCardInDOM(formData.get('content'), formData.get('list_id'));
     },
 
-    makeListInDOM: function(listName) {
+    makeListInDOM: function(listName, listId) {
         const templateList = document.getElementById('template-list');
         const templateListContent = templateList.content;
         // Création d'un nouvel objet DOM (clone) à partir du fragment de document récupéré précédemment
         const newList = document.importNode(templateListContent, true);
+        
         const listTitle = newList.querySelector('h2');
         listTitle.textContent = listName;
+        
+        listTitle.addEventListener('dblclick', app.showFormToEditTitle);
+        const form = listTitle.nextElementSibling;
+        form.addEventListener('submit', app.handleEditListForm);
+        
+        if (listId) {
+            // On doit d'abord aller chercher l'élément DOM global du fragment
+            const blockList = newList.querySelector('.panel');
+            // puis on remplace 'list-id' par l'id de la liste courante récupérée par l'API,
+            // comme ça les cartes ajoutées par la modal iront sur les listes ciblées
+            blockList.setAttribute('list-id', listId);
+            // Pour modifier la valeur du champs caché du formulaire caché permettant de modifier une liste
+            const idField = form.querySelector('input[name="id"]');
+            idField.value = listId;
+            // Option : prépremplir le champ name avec la valeur actuelle du titre
+            const nameField = form.querySelector('input[name="name"]');
+            nameField.value = listName;
+          }
         const listContainer = document.querySelector('.card-lists');
         // ciblage du bouton pour ajouter une carte dans la liste
         const button = newList.querySelector('.add-card-button');
@@ -78,21 +144,68 @@ const app = {
         button.addEventListener('click', app.showAddCardModal);
     },
 
-    makeCardInDOM: function(cardDescription, listId){
+    // params regroupés dans objet "card"
+    makeCardInDOM: function(card){
         const cardTemplate = document.getElementById('template-card');
         const templateCardContent = cardTemplate.content;
         const newCard = document.importNode(templateCardContent, true);
     
         const newCardContent = newCard.querySelector('.content');
-        newCardContent.textContent = cardDescription;
+        newCardContent.textContent = card.content;
     
-        const listContainer = document.querySelector('[list-id="'+listId+'"] .panel-block');
+        const listContainer = document.querySelector('[list-id="'+card.list_id+'"] .panel-block');
+        const cardBox = newCard.querySelector('.box');
+        cardBox.style.backgroundColor = card.color;
+        
         listContainer.append(newCard);
     
         app.hideModals();
-      },
+    },
   
-  };
+    showFormToEditTitle: function(event) {
+        // pour recup le titre double-cliqué
+        const currentTitle = event.target;
+        // pour cacher le titre double-cliqué
+        currentTitle.classList.add('is-hidden');
+        // pour afficher le form à la place -> nextSibling sélectionne l'élément suivant du DOM
+        const showForm = currentTitle.nextElementSibling;
+        showForm.classList.remove('is-hidden');
+    },
+
+    handleEditListForm: async function(event) {
+        try {
+            event.preventDefault();
+
+            const formData = new FormData(event.target);
+            // ne pas oublier d'ajouter l'id de la route pour modifier la liste
+            const response = await fetch(`${app.base_url}/lists/${formData.get('id')}`, {
+                method: 'PATCH',
+                body: formData
+            });
+
+            const newListOrError = await response.json();
+            
+            if(response.status !== 200){
+                throw Error(newListOrError);
+            }
+
+            // doublon quand on modifie le titre avec :
+            // app.makeListInDOM(newListOrError.name, newListOrError.id);
+            // il faut directement modifier le nom via les données envoyées dans le formulaire
+            // donc on récup le code de showFormToEditTitle et on inverse
+            const currentList = event.target.closest('.panel');
+            const currentTitle = currentList.querySelector('h2');
+            currentTitle.textContent = newListOrError.name;
+            currentTitle.classList.remove('is-hidden');
+            const showForm = currentTitle.nextElementSibling;
+            showForm.classList.add('is-hidden');
+
+        } catch (error) {
+            alert('désolé, une erreur est survenue..');
+            console.error(error);
+        }
+    },
+};
   
-  // Ecouteur d'évènement sur le doc : quand le chargement est terminé -> app.init
-  document.addEventListener('DOMContentLoaded', app.init );
+    // Ecouteur d'évènement sur le doc : quand le chargement est terminé -> app.init
+    document.addEventListener('DOMContentLoaded', app.init );
